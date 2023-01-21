8ca5d8b65f03f6371263dc6c4503d660e23ce4dd
JObog <jobogbaimhe@gmail.com>
2023-01-20 23:26:46 -0500

    added redis cache impl

--- server/cache/cache.go
+++ server/cache/cache.go
@@ -1,6 +1,6 @@
 package cache
 
 type Cache interface {
-	Get(key string) *string
-	Set(key string, value string, ttl int64)
+	Get(key string) (*string, error)
+	Set(key string, value string, ttl int64) error
 }
