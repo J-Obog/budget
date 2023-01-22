cdb055b3617ffab21567e90d5a03272d9331d295
JObog <jobogbaimhe@gmail.com>
2023-01-22 13:02:19 -0500

    updating resource interfaces' method sigs

--- server/resource/resource.go
+++ server/resource/resource.go
@@ -1,27 +1,27 @@
 package resource
 
 type AuthResource interface {
-	Login()
-	Refresh()
-	Revoke()
+	Login(req Request) Response
+	Refresh(req Request) Response
+	Revoke(req Request) Response
 }
 
 type CategoryResource interface {
-	GetCategory()
-	GetCategories()
+	GetCategory(req Request) Response
+	GetCategories(req Request) Response
 }
 
 type GoalResource interface {
-	GetGoal()
-	GetGoals()
-	UpdateGoal()
-	CreateGoal()
-	DeleteGoal()
+	GetGoal(req Request) Response
+	GetGoals(req Request) Response
+	UpdateGoal(req Request) Response
+	CreateGoal(req Request) Response
+	DeleteGoal(req Request) Response
 }
 
 type Account interface {
-	GetAccount()
-	UpdateAccount()
-	CreateAccount()
-	DeleteAccount()
+	GetAccount(req Request) Response
+	UpdateAccount(req Request) Response
+	CreateAccount(req Request) Response
+	DeleteAccount(req Request) Response
 }
