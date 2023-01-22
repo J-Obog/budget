fe9aaa4d8dd8338b68b70066b6deba09c15c79ee
JObog <jobogbaimhe@gmail.com>
2023-01-22 16:51:27 -0500

    adding base create account endpoint impl

--- server/resource/account.go
+++ server/resource/account.go
@@ -47,7 +47,54 @@ func (this *AccountResource) UpdateAccount(req Request) *Response {
 }
 
 func (this *AccountResource) CreateAccount(req Request) *Response {
-	return nil
+	var accountCreateRequest AccountCreateRequest
+
+	err := json.Unmarshal(req.Body, &accountCreateRequest)
+
+	if err != nil {
+		//return 500
+	}
+
+	//do validations
+
+	account, err := this.accountStore.GetByEmail(accountCreateRequest.Email)
+
+	if err != nil {
+		//return 500
+	}
+
+	if account != nil {
+		//return 400 email already taken
+	}
+
+	timeNow := int64(123) //dummy current time
+
+	newAccount := db.Account{
+		Id:                   "generated-uuid",
+		Email:                accountCreateRequest.Email,
+		Password:             accountCreateRequest.Password, //make sure this gets encrypted
+		NotificationsEnabled: false,
+		CreatedAt:            timeNow,
+		UpdatedAt:            timeNow,
+	}
+
+	err = this.accountStore.Insert(newAccount)
+
+	if err != nil {
+		//return 500
+	}
+
+	accountResponse := this.toAccountResponse(newAccount)
+	responseBody, err := json.Marshal(accountResponse)
+
+	if err != nil {
+		//return 500
+	}
+
+	return &Response{
+		Body:   responseBody,
+		Status: http.StatusOK,
+	}
 }
 
 func (this *AccountResource) DeleteAccount(req Request) *Response {
