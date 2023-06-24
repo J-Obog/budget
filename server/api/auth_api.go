4136333aced2050b3330a6d22321868b5c973b5a
J-Obog <jobogbaimhe@gmail.com>
2023-06-24 14:58:16 -0400

    updating auth api

--- server/api/auth_api.go
+++ server/api/auth_api.go
@@ -1,4 +1,26 @@
 package api
 
+import "github.com/J-Obog/paidoff/data"
+
 type AuthAPI struct {
+}
+
+func (api *AuthAPI) Login(req *data.RestRequest, res *data.RestResponse) error {
+	return nil
+}
+
+func (api *AuthAPI) Register(req *data.RestRequest, res *data.RestResponse) error {
+	return nil
+}
+
+func (api *AuthAPI) Logout(req *data.RestRequest, res *data.RestResponse) error {
+	return nil
+}
+
+func (api *AuthAPI) Refresh(req *data.RestRequest, res *data.RestResponse) error {
+	return nil
+}
+
+func (api *AuthAPI) ConfirmEmail(req *data.RestRequest, res *data.RestResponse) error {
+	return nil
 }
--- server/api/transaction_api.go
+++ server/api/transaction_api.go
@@ -5,18 +5,18 @@ import "github.com/J-Obog/paidoff/data"
 type TransactionAPI struct {
 }
 
-func (api *TransactionAPI) GetCategory(req *data.RestRequest, res *data.RestResponse) error {
+func (api *TransactionAPI) GetTransaction(req *data.RestRequest, res *data.RestResponse) error {
 	return nil
 }
 
-func (api *TransactionAPI) CreateCategory(req *data.RestRequest, res *data.RestResponse) error {
+func (api *TransactionAPI) CreateTransaction(req *data.RestRequest, res *data.RestResponse) error {
 	return nil
 }
 
-func (api *TransactionAPI) UpdateCategory(req *data.RestRequest, res *data.RestResponse) error {
+func (api *TransactionAPI) UpdateTransaction(req *data.RestRequest, res *data.RestResponse) error {
 	return nil
 }
 
-func (api *TransactionAPI) DeleteCategory(req *data.RestRequest, res *data.RestResponse) error {
+func (api *TransactionAPI) DeleteTransaction(req *data.RestRequest, res *data.RestResponse) error {
 	return nil
 }
