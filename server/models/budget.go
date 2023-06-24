c26d0243d9246db1dbbde5af1d18aeb946bb9013
J-Obog <jobogbaimhe@gmail.com>
2023-06-24 13:01:13 -0400

    adding budget type enum

--- server/models/budget.go
+++ server/models/budget.go
@@ -1,15 +1,22 @@
 package models
 
+type BudgetType uint
+
+const (
+	BudgetType_INCOME  BudgetType = 0
+	BudgetType_EXPENSE BudgetType = 1
+)
+
 type Budget struct {
 	Id         string
 	AccountId  string
 	CategoryId *string
 	Name       string
-	//Type: BudgetType
-	Month     int
-	Year      int
-	Projected float64
-	Actual    float64
-	CreatedAt int
-	UpdatedAt int
+	Type       BudgetType
+	Month      int
+	Year       int
+	Projected  float64
+	Actual     float64
+	CreatedAt  int
+	UpdatedAt  int
 }
