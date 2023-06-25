a51ba22bd12e2234f1794b28f4f4f3a71742a0c3
J-Obog <jobogbaimhe@gmail.com>
2023-06-25 18:40:54 -0400

    #1 adding test file for store

---
+++ server/store/store_test.go
@@ -0,0 +1,20 @@
+package store
+
+import (
+	"testing"
+)
+
+func TestAccounts(t *testing.T) {
+	t.Run("inserts and gets an account", func(t *testing.T) {
+
+	})
+
+	t.Run("updates an account", func(t *testing.T) {
+
+	})
+
+	t.Run("deletes an account", func(t *testing.T) {
+
+	})
+
+}
