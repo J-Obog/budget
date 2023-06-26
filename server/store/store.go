616b0b31f7c90d5597a1a7b56791e0e206d33e48
J-Obog <jobogbaimhe@gmail.com>
2023-06-25 22:15:57 -0400

    adding budget it tests

--- server/store/store.go
+++ server/store/store.go
@@ -9,18 +9,18 @@ type Store interface {
 	DeleteAccount(id string) error
 
 	GetCategory(id string) (*data.Category, error)
-	InsertCategory(account data.Category) error
-	UpdateCategory(account data.Category) error
+	InsertCategory(category data.Category) error
+	UpdateCategory(category data.Category) error
 	DeleteCategory(id string) error
 
 	GetBudget(id string) (*data.Budget, error)
-	InsertBudget(account data.Budget) error
-	UpdateBudget(account data.Budget) error
+	InsertBudget(budget data.Budget) error
+	UpdateBudget(budget data.Budget) error
 	DeleteBudget(id string) error
 
 	GetTransaction(id string) (*data.Transaction, error)
-	InsertTransaction(account data.Transaction) error
-	UpdateTransaction(account data.Transaction) error
+	InsertTransaction(transaction data.Transaction) error
+	UpdateTransaction(transaction data.Transaction) error
 	DeleteTransaction(id string) error
 
 	Flush() error
--- server/store/store_test.go
+++ server/store/store_test.go
@@ -8,10 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	testAccountId = "test-account-1"
-)
-
 func setup(t *testing.T, store Store) {
 	err := store.Flush()
 	assert.NoError(t, err)
@@ -39,31 +35,51 @@ func makeAccount() data.Account {
 	}
 }
 
+func makeBudget() data.Budget {
+	return data.Budget{
+		Id:         "test-123",
+		AccountId:  "test-456",
+		CategoryId: nil,
+		Name:       "eating",
+		Type:       data.BudgetType_EXPENSE,
+		Month:      8,
+		Year:       2023,
+		Projected:  115.60,
+		Actual:     67.89,
+		CreatedAt:  123,
+		UpdatedAt:  456,
+	}
+}
+
 func TestAccounts(t *testing.T) {
 	store := getStore(t)
 
-	t.Run("inserts and gets an account", func(t *testing.T) {
+	t.Run("it inserts and gets", func(t *testing.T) {
 		setup(t, store)
 
+		testId := "testing-123"
+
 		account := makeAccount()
-		account.Id = testAccountId
+		account.Id = testId
 
 		err := store.InsertAccount(account)
 		assert.NoError(t, err)
 
-		fetchedAccount, err := store.GetAccount(testAccountId)
+		fetchedAccount, err := store.GetAccount(testId)
 		assert.NoError(t, err)
 		assert.Equal(t, account, *fetchedAccount)
 	})
 
-	t.Run("updates an account", func(t *testing.T) {
+	t.Run("it updates", func(t *testing.T) {
 		setup(t, store)
+
+		testId := "testing-123"
 
 		oldEmail := "jdoe@gmail.com"
 		newEmail := "jdoe@yahoo.com"
 
 		account := makeAccount()
-		account.Id = testAccountId
+		account.Id = testId
 		account.Email = oldEmail
 
 		err := store.InsertAccount(account)
@@ -74,26 +90,92 @@ func TestAccounts(t *testing.T) {
 		err = store.UpdateAccount(account)
 		assert.NoError(t, err)
 
-		fetchedAccount, err := store.GetAccount(testAccountId)
+		fetchedAccount, err := store.GetAccount(testId)
 		assert.NoError(t, err)
 		assert.Equal(t, account, *fetchedAccount)
 	})
 
-	t.Run("deletes an account", func(t *testing.T) {
+	t.Run("it deletes", func(t *testing.T) {
 		setup(t, store)
 
+		testId := "testing-123"
+
 		account := makeAccount()
-		account.Id = testAccountId
+		account.Id = testId
 
 		err := store.InsertAccount(account)
 		assert.NoError(t, err)
 
-		err = store.DeleteAccount(testAccountId)
+		err = store.DeleteAccount(testId)
 		assert.NoError(t, err)
 
-		fetchedAccount, err := store.GetAccount(testAccountId)
+		fetchedAccount, err := store.GetAccount(testId)
 		assert.NoError(t, err)
 
 		assert.Nil(t, fetchedAccount)
+	})
+}
+
+func TestBudgets(t *testing.T) {
+	store := getStore(t)
+
+	t.Run("it inserts and gets", func(t *testing.T) {
+		setup(t, store)
+
+		testId := "test-1"
+
+		budget := makeBudget()
+		budget.Id = testId
+
+		err := store.InsertBudget(budget)
+		assert.NoError(t, err)
+
+		fetchedBudget, err := store.GetBudget(testId)
+		assert.NoError(t, err)
+		assert.Equal(t, budget, *fetchedBudget)
+	})
+
+	t.Run("it updates", func(t *testing.T) {
+		setup(t, store)
+
+		oldProjectedBudget := 12345
+		newProjectedBudget := 50000
+
+		testId := "t-123456"
+
+		budget := makeBudget()
+		budget.Id = testId
+		budget.Projected = float64(oldProjectedBudget)
+
+		err := store.InsertBudget(budget)
+		assert.NoError(t, err)
+
+		budget.Projected = float64(newProjectedBudget)
+
+		err = store.UpdateBudget(budget)
+		assert.NoError(t, err)
+
+		fetchedBudget, err := store.GetBudget(testId)
+		assert.NoError(t, err)
+		assert.Equal(t, budget, *fetchedBudget)
+	})
+
+	t.Run("it deletes", func(t *testing.T) {
+		setup(t, store)
+
+		testId := "t-1"
+
+		budget := makeBudget()
+		budget.Id = testId
+
+		err := store.InsertBudget(budget)
+		assert.NoError(t, err)
+
+		err = store.DeleteBudget(testId)
+		assert.NoError(t, err)
+
+		fetchedBudget, err := store.GetBudget(testId)
+		assert.NoError(t, err)
+		assert.Nil(t, fetchedBudget)
 	})
 }
