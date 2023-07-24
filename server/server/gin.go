997315648e895827e1450e1b612b39a3fcf581b2
J-Obog <jobogbaimhe@gmail.com>
2023-07-24 11:49:43 -0400

    gin deps

--- server/server/gin.go
+++ server/server/gin.go
@@ -1,13 +1,49 @@
 package server
 
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
 type GinServer struct {
 	BaseServer
+	eng *gin.Engine
 }
 
-func (gin *GinServer) Start(port int) error {
+func (g *GinServer) Start(port int) error {
+	g.eng.GET("/account")
+	g.eng.PUT("/account")
+	g.eng.DELETE("/account")
+
+	g.eng.GET("/category")
+	g.eng.GET("/category/:id")
+	g.eng.POST("/category")
+	g.eng.PUT("/category/:id")
+	g.eng.DELETE("/category/:id")
+
+	g.eng.GET("/transaction")
+	g.eng.GET("/transaction/:id")
+	g.eng.POST("/transaction")
+	g.eng.PUT("/transaction/:id")
+	g.eng.DELETE("/transaction/:id")
+
+	g.eng.GET("/budget")
+	g.eng.GET("/budget/:id")
+	//g.eng.GET("/budget/types")
+	g.eng.POST("/budget")
+	g.eng.PUT("/budget/:id")
+	g.eng.DELETE("/budget/:id")
+
+	return g.eng.Run(fmt.Sprintf(":%d", port))
+}
+
+func (g *GinServer) Stop() error {
 	return nil
 }
 
-func (gin *GinServer) Stop() error {
-	return nil
+func (g *GinServer) handle() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
 }
--- server/server/server.go
+++ server/server/server.go
@@ -1,5 +1,9 @@
 package server
 
+import "github.com/J-Obog/paidoff/rest"
+
+type routeHandler func(*rest.Request) *rest.Response
+
 type Server interface {
 	Start(port int) error
 	Stop() error
