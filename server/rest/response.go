4d463352821e08952ae19cc5f4b8a861f584cf52
J-Obog <jobogbaimhe@gmail.com>
2023-07-24 22:00:34 -0400

    adding function to convert response to json

--- server/rest/response.go
+++ server/rest/response.go
@@ -1,5 +1,10 @@
 package rest
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Response struct {
 	Data  any
 	Error error
@@ -19,4 +24,25 @@ func Err(err error) *Response {
 	return &Response{
 		Error: err,
 	}
+}
+
+func (res *Response) ToJSON() ([]byte, int) {
+	if res.Error != nil {
+		restErr, isHandledError := res.Error.(*RestError)
+
+		if isHandledError {
+			d := []byte(fmt.Sprintf(`{"error": {"message": %s}}`, restErr.Error()))
+			return d, restErr.Status
+		}
+
+		return []byte(`{"error": {"message": "internal error"}}`), 500
+	}
+
+	b, err := json.Marshal(res.Data)
+
+	if err != nil {
+		return []byte(`{"error": {"message": "internal error"}}`), 500
+	}
+
+	return b, 200
 }
