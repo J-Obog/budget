bfe43382fc5eb91ce9f4d972bd454b81434db37c
J-Obog <jobogbaimhe@gmail.com>
2023-08-05 18:56:48 -0400

    bringing back account manager

--- server/api/account_api.go
+++ server/api/account_api.go
@@ -1,38 +1,28 @@
 package api
 
 import (
-	"github.com/J-Obog/paidoff/clock"
 	"github.com/J-Obog/paidoff/config"
 	"github.com/J-Obog/paidoff/data"
+	"github.com/J-Obog/paidoff/manager"
 	"github.com/J-Obog/paidoff/rest"
-	"github.com/J-Obog/paidoff/store"
 )
 
 type AccountAPI struct {
-	accountStore store.AccountStore
-	clock        clock.Clock
+	accountManager manager.AccountManager
 }
 
-func NewAccountAPI(
-	accountStore store.AccountStore,
-	clock clock.Clock,
-) *AccountAPI {
+func NewAccountAPI(accountManager manager.AccountManager) *AccountAPI {
 	return &AccountAPI{
-		accountStore: accountStore,
-		clock:        clock,
+		accountManager: accountManager,
 	}
 }
 
 func (api *AccountAPI) Get(req *rest.Request) *rest.Response {
 	accountId := testAccountId
 
-	account, err := api.accountStore.Get(accountId)
+	account, err := api.accountManager.Get(accountId)
 	if err != nil {
-		return rest.Err(err)
-	}
-
-	if account == nil {
-		return rest.Err(rest.ErrInvalidAccountId)
+		rest.Err(err)
 	}
 
 	return rest.Ok(account)
@@ -40,14 +30,13 @@ func (api *AccountAPI) Get(req *rest.Request) *rest.Response {
 
 func (api *AccountAPI) Update(req *rest.Request) *rest.Response {
 	accountId := testAccountId
-	timestamp := api.clock.Now()
 
 	body, err := rest.ParseBody[rest.AccountUpdateBody](req.Body)
 	if err != nil {
 		return rest.Err(err)
 	}
 
-	account, err := api.accountStore.Get(accountId)
+	account, err := api.accountManager.Get(accountId)
 	if err != nil {
 		return rest.Err(err)
 	}
@@ -56,19 +45,8 @@ func (api *AccountAPI) Update(req *rest.Request) *rest.Response {
 		return rest.Err(err)
 	}
 
-	update := data.AccountUpdate{
-		Name: body.Name,
-	}
-
-	account.Name = update.Name
-
-	ok, err := api.accountStore.Update(accountId, update, timestamp)
-	if err != nil {
-		return rest.Err(rest.ErrInternalServer)
-	}
-
-	if !ok {
-		return rest.Err(rest.ErrInvalidAccountId)
+	if err := api.accountManager.Update(account, body); err != nil {
+		return rest.Err(err)
 	}
 
 	return rest.Ok(account)
@@ -77,23 +55,14 @@ func (api *AccountAPI) Update(req *rest.Request) *rest.Response {
 func (api *AccountAPI) Delete(req *rest.Request) *rest.Response {
 	accountId := testAccountId
 
-	ok, err := api.accountStore.Delete(accountId)
-	if err != nil {
-		return rest.Err(rest.ErrInternalServer)
-	}
-
-	if !ok {
-		return rest.Err(rest.ErrInvalidAccountId)
+	if err := api.accountManager.Delete(accountId); err != nil {
+		return rest.Err(err)
 	}
 
 	return rest.Success()
 }
 
 func (api *AccountAPI) validateUpdate(existing *data.Account, body rest.AccountUpdateBody) error {
-	if existing == nil {
-		return rest.ErrInvalidAccountId
-	}
-
 	if body.Name != existing.Name {
 		if err := api.checkAccountName(body.Name); err != nil {
 			return err
