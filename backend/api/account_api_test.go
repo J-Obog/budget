1150895b1161b94c3016ab538cd5563cba4a1259
J-Obog <jobogbaimhe@gmail.com>
2023-08-12 19:55:24 -0400

    some testing

--- backend/api/account_api_test.go
+++ backend/api/account_api_test.go
@@ -22,7 +22,6 @@ func (s *AccountApiTestSuite) SetupTest() {
 
 func (s *AccountApiTestSuite) TestGets() {
 	s.accountStore.Insert(data.Account{Id: testAccountId})
-
 	req := &rest.Request{}
 	res := s.api.Get(req)
 	s.OkResponse(res, data.Account{})
@@ -36,7 +35,6 @@ func (s *AccountApiTestSuite) TestGetFailsIfNoAccountExists() {
 
 func (s *AccountApiTestSuite) TestUpdates() {
 	s.accountStore.Insert(data.Account{Id: testAccountId})
-
 	reqBody := rest.AccountUpdateBody{Name: "New Name"}
 	req := &rest.Request{Body: s.getJSONBody(reqBody)}
 	res := s.api.Update(req)
@@ -52,28 +50,18 @@ func (s *AccountApiTestSuite) TestUpdateFailsIfNoAccountExists() {
 
 func (s *AccountApiTestSuite) TestUpdateFailsIfAccountNameIsInvalid() {
 	s.accountStore.Insert(data.Account{Id: testAccountId})
+	invalidNames := []string{}
 
-	cases := []struct {
-		scenario    string
-		accountName string
-	}{
-		{"account name too short", ""},
-		{"account name too long", ""},
-	}
-
-	for _, testCase := range cases {
-		s.Run(testCase.scenario, func() {
-			reqBody := rest.AccountUpdateBody{Name: testCase.accountName}
-			req := &rest.Request{Body: s.getJSONBody(reqBody)}
-			res := s.api.Update(req)
-			s.ErrRepsonse(res, rest.ErrInvalidAccountName)
-		})
+	for _, name := range invalidNames {
+		reqBody := rest.AccountUpdateBody{Name: name}
+		req := &rest.Request{Body: s.getJSONBody(reqBody)}
+		res := s.api.Update(req)
+		s.ErrRepsonse(res, rest.ErrInvalidAccountName)
 	}
 }
 
 func (s *AccountApiTestSuite) TestDeletes() {
 	s.accountStore.Insert(data.Account{Id: testAccountId})
-
 	req := &rest.Request{}
 	res := s.api.Delete(req)
 	s.OkResponse(res, rest.Success().Data)
--- backend/rest/body.go
+++ backend/rest/body.go
@@ -7,7 +7,26 @@ import (
 
 type JSONBody []byte
 
+func (j JSONBody) getRestError(err error) *RestError {
+	switch jsonErr := err.(type) {
+	case *json.SyntaxError:
+		return ErrInvalidJSONBody
+	case *json.UnmarshalTypeError:
+		return &RestError{Msg: fmt.Sprintf("invalid value for %s", jsonErr.Field)}
+	default:
+		return ErrInternalServer
+	}
+}
+
 func (j JSONBody) From(obj any) error {
+	bytes, err := json.Marshal(obj)
+
+	if err != nil {
+		return j.getRestError(err)
+	}
+
+	j = bytes
+
 	return nil
 }
 
@@ -15,14 +34,7 @@ func (j JSONBody) To(obj any) error {
 	err := json.Unmarshal(j, obj)
 
 	if err != nil {
-		switch jsonErr := err.(type) {
-		case *json.SyntaxError:
-			return ErrInvalidJSONBody
-		case *json.UnmarshalTypeError:
-			return &RestError{Msg: fmt.Sprintf("invalid value for %s", jsonErr.Field)}
-		default:
-			return ErrInternalServer
-		}
+		return j.getRestError(err)
 	}
 
 	return nil
