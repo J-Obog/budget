a8d7636e32e806f7385471e234d0abd753e77edf
J-Obog <jobogbaimhe@gmail.com>
2023-08-21 19:45:43 -0400

    adding another test case for category deletion worker

--- backend/workers/category_deletion_worker_test.go
+++ backend/workers/category_deletion_worker_test.go
@@ -64,3 +64,37 @@ func (s *CategoryDeletionWorkerTestSuite) TestNullsCategoryIds() {
 	s.NotNil(transaction)
 	s.Nil(transaction.CategoryId)
 }
+
+func (s *CategoryDeletionWorkerTestSuite) TestDoesntNullCategoryIdsIfCategoryIsntDeleted() {
+	transactionId := "some-cool-id"
+	accountId := "acct-1234"
+	categoryId := "some-cat-id"
+
+	err := s.transactionStore.Insert(data.Transaction{
+		Id:         transactionId,
+		AccountId:  accountId,
+		CategoryId: types.StringPtr(categoryId),
+	})
+
+	s.NoError(err)
+
+	err = s.categoryStore.Insert(data.Category{
+		Id:        categoryId,
+		AccountId: accountId,
+	})
+
+	s.NoError(err)
+
+	msg := queue.CategoryDeletedMessage{CategoryId: categoryId, AccountId: accountId}
+
+	err = s.queue.Push(queue.ToMessage("some-uuid", &msg), queue.QueueName_CategoryDeleted)
+	s.NoError(err)
+
+	err = s.worker.Work()
+	s.NoError(err)
+
+	transaction, err := s.transactionStore.Get(transactionId, accountId)
+	s.NoError(err)
+	s.NotNil(transaction)
+	s.NotNil(transaction.CategoryId)
+}
